326ef1725f947861badf136ef800342cdf30ffde
Chromus-dev <xlchannel6@gmail.com>
2024-11-07 13:17:03 -0800

    remove resend

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,11 +1,8 @@
-import { Resend } from 'resend';
 import nodemailer from 'nodemailer';
 
 //! migrate to nodemailer so I can use gmail
 // https://www.youtube.com/watch?app=desktop&v=oujAYWgYwtM&sttick=0
 // https://github.com/zinotrust/nextjs-email-tutorial/blob/master/app/api/sendEmail/route.js
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 const domain = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -31,7 +28,6 @@ export const sendEmail = async (
 			to: email,
 			subject: subject,
 			html: html,
-			// text: message,
 		};
 
 		await transporter.sendMail(mailOption);
@@ -49,13 +45,6 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 		'Verify your email address',
 		`<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
 	);
-
-	// await resend.emails.send({
-	// 	from: 'mail@mrigankagogoi.com',
-	// 	to: email,
-	// 	subject: 'Verify your email address',
-	// 	html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
-	// });
 };
 
 // Send password reset email
@@ -67,23 +56,9 @@ export const sendPasswordResetmail = async (email: string, token: string) => {
 		'Reset your password',
 		`<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
 	);
-
-	// await resend.emails.send({
-	// 	from: 'mail@mrigankagogoi.com',
-	// 	to: email,
-	// 	subject: 'Reset your password',
-	// 	html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
-	// });
 };
 
 // Send two factor token
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 	await sendEmail(email, '2FA Code', `<p>Your 2FA code is: ${token}</p>`);
-
-	// await resend.emails.send({
-	// 	from: 'mail@mrigankagogoi.com',
-	// 	to: email,
-	// 	subject: '2FA Code',
-	// 	html: `<p>Your 2FA code is: ${token}</p>`,
-	// });
 };
